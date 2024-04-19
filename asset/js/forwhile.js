c84bf3184c38b921826e1c31a43451a7fb755b8c
The_Lost_Owl <60459107+vungoctuyen1812@users.noreply.github.com>
2024-04-19 11:37:59 +0700

    Nop bai tap vong lap for-while

---
+++ asset/js/forwhile.js
@@ -0,0 +1,33 @@
+//Bài 1: In ra các số chẵn từ 1 -> 100 dùng for và while
+//Dùng for
+for(let i = 1; i <= 100; i++) {
+    if(i%2 == 0) {
+        console.log(i)
+    }
+}
+
+// Dùng while
+let i2 = 1
+while(i2 <= 100) {
+    if( i2%2 == 0){
+    console.log(i2)
+    }
+    i2++;
+}
+
+// Bài 2: In ra các số lẻ từ 1 -> 100 dùng for và while
+// Dùng for
+for(let a = 1; a <= 100; a++) {
+    if(a%2 == 1) {
+        console.log(a)
+    }
+}
+
+//Dùng while
+let a2 = 1
+while(a2 <= 100) {
+    if( a2%2 == 1){
+    console.log(a2)
+    }
+    a2++;
+}
